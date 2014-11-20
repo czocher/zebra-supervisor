6e6bc1ebd7ca078c7da819aee87431d3cc849edf
Paweł Jan Czochański <czochanski@gmail.com>
2014-11-20 11:20:08 +0100

    Moved from fixtures to data migrations.

---
+++ judge/migrations/0001_initial.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('rest', '__first__'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ConfigTest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Timestamp')),
+                ('file', models.FileField(upload_to=b'tests/', verbose_name='Tests File')),
+            ],
+            options={
+                'verbose_name': 'Test config',
+                'verbose_name_plural': 'Test configs',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Contest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
+                ('start_time', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Start time')),
+                ('freeze_time', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Freeze time')),
+                ('end_time', models.DateTimeField(default=django.utils.timezone.now, verbose_name='End time')),
+                ('team', models.BooleanField(default=False, verbose_name='Team contest')),
+                ('penalty', models.IntegerField(default=0, verbose_name='Penalty for wrong submissions')),
+                ('printing', models.BooleanField(default=False, verbose_name='Is printing avaliable')),
+            ],
+            options={
+                'ordering': ['name'],
+                'verbose_name': 'Contest',
+                'verbose_name_plural': 'Contests',
+                'permissions': (('view_contest', 'Can view Contest'), ('see_unfrozen_ranking', 'Can see unfrozen ranking')),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='InputTest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Timestamp')),
+                ('file', models.FileField(upload_to=b'tests/', verbose_name='Tests File')),
+            ],
+            options={
+                'verbose_name': 'Test input',
+                'verbose_name_plural': 'Test inputs',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='OutputTest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Timestamp')),
+                ('file', models.FileField(upload_to=b'tests/', verbose_name='Tests File')),
+            ],
+            options={
+                'verbose_name': 'Test output',
+                'verbose_name_plural': 'Test outputs',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Problem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('codename', models.SlugField(help_text="Example: 'FIB01'. A short text to identify this problem, used as an id for urls.", unique=True, max_length=10, verbose_name='Codename')),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
+                ('content', models.TextField(default=b'\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nCurabitur id tortor at dui porta sollicitudin in sit amet nisi.\nNunc venenatis tincidunt orci eget imperdiet.\nMaecenas rutrum arcu at nunc aliquet pulvinar.\nMaecenas blandit augue nec augue semper id ornare lorem congue.\nEtiam non erat lorem, eget semper eros.</p>\n\n<h3>Input</h3>\nOn the input the program recives two binary numbers.\n\n<h3>Output</h3>\nOn the output the program should print a binary number.\n', verbose_name='Content')),
+            ],
+            options={
+                'verbose_name': 'Problem',
+                'verbose_name_plural': 'Problems',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Result',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('returncode', models.IntegerField(default=0, verbose_name='Return code')),
+                ('mark', models.BooleanField(default=True, verbose_name='Mark')),
+                ('time', models.FloatField(default=0, verbose_name='Execution time')),
+            ],
+            options={
+                'ordering': ['time'],
+                'verbose_name': 'Result',
+                'verbose_name_plural': 'Results',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SampleIO',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('input', models.TextField(verbose_name='Input')),
+                ('output', models.TextField(verbose_name='Output')),
+                ('problem', models.ForeignKey(verbose_name='Problem', to='judge.Problem')),
+            ],
+            options={
+                'verbose_name': 'Sample I/O',
+                'verbose_name_plural': 'Sample I/O',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Submission',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('source', models.TextField(verbose_name='Source code')),
+                ('language', models.CharField(max_length=10, verbose_name='Language', choices=[(b'c', b'C'), (b'cpp', b'C++'), (b'pas', b'Pascal'), (b'py', b'Python'), (b'java', b'Java')])),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Send time')),
+                ('compilelog', models.TextField(default=b'', null=True, verbose_name='Compilation log', blank=True)),
+                ('score', models.IntegerField(default=-1, verbose_name='Score')),
+                ('status', models.IntegerField(default=1, verbose_name='Status', choices=[(2, 'Judging'), (1, 'Waiting'), (3, 'Judged')])),
+                ('author', models.ForeignKey(related_name='submissions', verbose_name='Author', to=settings.AUTH_USER_MODEL)),
+                ('contest', models.ForeignKey(related_name='submissions', verbose_name='Contest', blank=True, to='judge.Contest', null=True)),
+                ('node', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, verbose_name='Node', blank=True, to='rest.Node', null=True)),
+                ('problem', models.ForeignKey(related_name='submissions', verbose_name='Problem', to='judge.Problem')),
+            ],
+            options={
+                'ordering': ['-timestamp'],
+                'verbose_name': 'Submission',
+                'verbose_name_plural': 'Submissions',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tests',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('config', models.OneToOneField(related_name='config', verbose_name='Config', to='judge.ConfigTest')),
+                ('input', models.OneToOneField(related_name='input', verbose_name='Inputs', to='judge.InputTest')),
+                ('output', models.OneToOneField(related_name='output', verbose_name='Outputs', to='judge.OutputTest')),
+                ('problem', models.OneToOneField(verbose_name='Problem', to='judge.Problem')),
+            ],
+            options={
+                'verbose_name': 'Tests',
+                'verbose_name_plural': 'Tests',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='result',
+            name='submission',
+            field=models.ForeignKey(related_name='results', verbose_name='Submission', to='judge.Submission'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contest',
+            name='problems',
+            field=models.ManyToManyField(related_name='contests', verbose_name='Problems', to='judge.Problem'),
+            preserve_default=True,
+        ),
+    ]
---
+++ judge/migrations/0002_initial_data.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.core import serializers
+from django.conf import settings
+import os
+
+
+FIXTURE_DIR = getattr(settings, 'FIXTURE_DIR', os.path.dirname(__file__))
+
+
+def get_fixture_file_paths():
+    fixture_file_paths = []
+
+    for name in os.listdir(FIXTURE_DIR):
+        path = os.path.join(FIXTURE_DIR, name)
+        if os.path.isfile(path) and path.endswith(('xml', 'json', 'yaml')):
+            fixture_file_paths.append(path)
+
+    return fixture_file_paths
+
+
+def initial_data(apps, schema_editor):
+
+    for filepath in get_fixture_file_paths():
+
+        with open(filepath, 'r') as fixtures:
+            objects = serializers.deserialize(
+                os.path.splitext(filepath)[1][1:],
+                fixtures, ignorenonexistent=True)
+
+            for obj in objects:
+                obj.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('judge', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(initial_data),
+    ]
