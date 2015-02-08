688c09131b95fc16b2ae6e2d0c120d4d5b9af2ec
Paweł Jan Czochański <czochanski@gmail.com>
2015-02-08 13:26:11 +0100

    Fixed pep8 errors, fixed locale file settings.

--- zebra/default_settings.py
+++ zebra/default_settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-p = lambda *x: os.path.join(BASE_DIR, *x)
+
+
+def p(*x): return os.path.join(BASE_DIR, *x)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -149,6 +151,7 @@ LANGUAGES = (
 LOCALE_PATHS = (
     p('locale'),
     p('judge/locale'),
+    p('rest/locale'),
 )
 
 AUTHENTICATION_BACKENDS = (
