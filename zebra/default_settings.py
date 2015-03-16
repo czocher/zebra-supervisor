0b759f4654e1e1698e415fac41f05f6cffdb5084
Paweł Jan Czochański <czochanski@gmail.com>
2015-03-16 14:55:59 +0100

    Remove unused XMLRenderer.

--- zebra/default_settings.py
+++ zebra/default_settings.py
@@ -172,7 +172,6 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.XMLRenderer',
     ]
 }
 
