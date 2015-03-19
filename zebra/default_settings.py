1fb11e2059d6637825179c34688937e8a86989d6
Paweł Jan Czochański <czochanski@gmail.com>
2015-03-19 14:29:20 +0100

    Added logging.

--- zebra/default_settings.py
+++ zebra/default_settings.py
@@ -202,3 +202,34 @@ BOOTSTRAP3 = {
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ("[%(asctime)s] %(levelname)s "
+                       "[%(name)s:%(lineno)s] %(message)s"),
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'zebra.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'zebra': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
