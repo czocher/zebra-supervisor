5a345f6acc10c1b3ca8291dfc5eb34ef682761ce
Paweł Jan Czochański <czochanski@gmail.com>
2015-11-01 16:38:49 +0100

    Fedora install script.

--- install/fedora.sh
+++ install/fedora.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "Remember to change the database and DEBUG settings!"
-cp ../zebra/default_settings.py ../zebra/settings.py
-sudo pip install -r ../requirements.txt
-../manage.py compilemessages
-../manage.py makemigrations
-../manage.py migrate
-
+if [ "$PWD" != "/var/www/zebra-supervisor/install" ]; then
+  echo "Move zebra-supervisor to /var/www/ then rerun this script."
+else
+  sudo pip install -r ../requirements.txt
+  sudo dnf install httpd mod_wsgi
+  sudo cp ../zebra/default_settings.py ../zebra/settings.py
+  sudo cp ./zebra.conf /etc/httpd/conf.d/
+  sudo ../manage.py collectstatic
+  sudo ../manage.py compilemessages
+  sudo ../manage.py makemigrations
+  sudo ../manage.py migrate
+  sudo chown apache:apache -R ../*
+  sudo systemctl restart httpd
+fi
