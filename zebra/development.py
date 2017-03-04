6424d2995249fb6b890a1916bef28058e3e40dc4
Paweł Jan Czochański <czochanski@gmail.com>
2017-03-04 20:10:29 +0100

    Settings and middleware update

--- zebra/development.py
+++ zebra/development.py
@@ -40,7 +40,29 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            p('templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
+            ],
+        },
+    },
+]
 
 ALLOWED_HOSTS = []
 
@@ -66,18 +88,7 @@ INSTALLED_APPS = (
     'questions',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages',
-    'django.core.context_processors.request'
-)
-
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,12 +147,6 @@ SECURE_BROWSER_XSS_FILTER = True
 CSRF_COOKIE_HTTPONLY = True
 
 X_FRAME_OPTIONS = 'DENY'
-
-# Where to search for templates
-
-TEMPLATE_DIRS = (
-    p('templates'),
-)
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 
--- zebra/middleware.py
+++ zebra/middleware.py
@@ -5,14 +5,16 @@ import re
 
 
 class RequireLoginMiddleware(object):
-    def __init__(self):
+    def __init__(self, get_response):
+        self.get_response = get_response
         self.urls = tuple(
-            [re.compile(url) for url in settings.LOGIN_REQUIRED_URLS])
+                [re.compile(url) for url in settings.LOGIN_REQUIRED_URLS])
         self.require_login_path = getattr(
-            settings, 'LOGIN_URL', '/accounts/login/')
+                settings, 'LOGIN_URL', '/accounts/login/')
 
-    def process_request(self, request):
+    def __call__(self, request):
         for url in self.urls:
             if url.match(request.path) and request.user.is_anonymous():
                 return HttpResponseRedirect(
                     '{}?next={}'.format(self.require_login_path, request.path))
+        return self.get_response(request)
