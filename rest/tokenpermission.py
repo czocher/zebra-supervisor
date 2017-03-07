252ea37fe321149930ef023b5715e46826b61380
Paweł Jan Czochański <czochanski@gmail.com>
2017-03-07 15:56:34 +0100

    REST allows users to access their objects
    The REST interface didn't allow users to access their submissions. It
    created an issue with the AJAX submission score downloader which
    couldn't get the score. It should be fixed now.

--- rest/tokenpermission.py
+++ rest/tokenpermission.py
@@ -11,11 +11,6 @@ LOGGER = logging.getLogger(__name__)
 
 def is_authorized_node(request):
     """Return True if the node from the request is authorized."""
-    if not request.is_secure():
-        LOGGER.warning(
-            "Received an insecure REST request, HTTPS required."
-        )
-
     token = request.META.get('HTTP_X_TOKEN')
     ipaddr = request.META.get('REMOTE_ADDR')
 
@@ -39,23 +34,16 @@ def is_authorized_node(request):
     return True
 
 
-def is_authorized_user(request, obj):
+def is_authorized_user(request):
     """Return True if the object can be accessed by the given user."""
-    if not request.is_secure():
-        LOGGER.warning(
-            "Received an insecure REST request, HTTPS required."
-        )
-
     # If user is staff then give him full access
-    if request.user and request.user.is_staff:
-        return True
-    # If user is authenticated and is the owner of that commit,
-    # then give him read access
-    elif request.user and request.user.is_authenticated \
-            and request.user == getattr(obj, 'author', None) \
-            and request.method in permissions.SAFE_METHODS:
-        return True
-    return False
+    return request.user.is_staff or request.user.is_authenticated
+
+def is_author(request, obj):
+    """Return True if the user is the author of the given object
+    and wants to read."""
+    return request.user == getattr(obj, 'author', None) \
+        and request.method in permissions.SAFE_METHODS
 
 
 class TokenPermission(permissions.BasePermission):
@@ -63,9 +51,30 @@ class TokenPermission(permissions.BasePermission):
     """Class to grant persmissions based on a given HTTP request header."""
 
     def has_permission(self, request, view):
-        return (is_authorized_node(request) or
-                is_authorized_user(request, obj=None))
+        if not request.is_secure():
+            LOGGER.warning(
+                    "Received an insecure REST request, HTTPS required.")
+        # has_permission is always called before has_object_permission
+        # it has to return True if has_object_permission is meant to be called
+        # therefore return True if a user is not a node but is authenticated
+        # and asks for a concrete submission
+
+        if is_authorized_node(request):
+            return True
+
+        if request.user.is_staff:
+            return True
+
+        view_name = request.resolver_match.url_name
+        if request.user.is_authenticated and view_name == 'submission-detail':
+            return True
+
+        return False
 
     def has_object_permission(self, request, view, obj):
+        if not request.is_secure():
+            LOGGER.warning(
+                    "Received an insecure REST request, HTTPS required.")
         return (is_authorized_node(request) or
-                is_authorized_user(request, obj))
+                (is_authorized_user(request)
+                    and is_author(request, obj)))
