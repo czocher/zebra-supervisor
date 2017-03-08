a809ee2406049df0f85dade8edf921f63d0c9b01
Paweł Jan Czochański <czochanski@gmail.com>
2017-03-08 23:57:26 +0100

    Added Contest class tests
    Started implementing unit tests for the Contest class. Fixed missing
    clean method.

--- judge/tests/test_contest.py
+++ judge/tests/test_contest.py
@@ -1,20 +1,23 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from django.core.exceptions import ValidationError
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
+
 from django.utils import timezone
 
 from judge.models import Contest
+
+from django.contrib.auth.models import User
 
 
 class ContestTestCase(TestCase):
 
     def setUp(self):
-       self.yesterday = timezone.now() - timezone.timedelta(days=1)
-       self.now = timezone.now()
-       self.in_two_hours = timezone.now() + timezone.timedelta(hours=2)
-       self.tomorrow = timezone.now() + timezone.timedelta(days=1)
+        self.yesterday = timezone.now() - timezone.timedelta(days=1)
+        self.now = timezone.now()
+        self.in_two_hours = timezone.now() + timezone.timedelta(hours=2)
+        self.tomorrow = timezone.now() + timezone.timedelta(days=1)
 
     def test_not_started(self):
         """Contests which are not started have the right properties."""
@@ -31,10 +34,6 @@ class ContestTestCase(TestCase):
         self.assertFalse(contest.is_started)
         contest.clean()
         contest.save()
-
-    def test_str(self):
-        """Contests should have a valid str method return value."""
-        pass # TODO
 
     def test_started(self):
         """Contests which are started have the right properties."""
@@ -120,7 +119,6 @@ class ContestTestCase(TestCase):
         self.assertRaises(ValidationError, contest.clean)
         self.assertRaises(ValidationError, contest.save)
 
-
     def test_construction_freeze_after_end(self):
         """Contests can not have freeze time after its end time."""
 
@@ -133,14 +131,91 @@ class ContestTestCase(TestCase):
         self.assertRaises(ValidationError, contest.clean)
         self.assertRaises(ValidationError, contest.save)
 
+    @override_settings(PRINTING_AVAILABLE=True)
+    def test_printing_available(self):
+        """Contests should allow printing when it is active, printing is
+        enabled and global switch if on."""
+
+        contest = Contest(
+            name="Test contest",
+            printing=True,
+            start_time=self.yesterday,
+            freeze_time=self.tomorrow,
+            end_time=self.tomorrow
+        )
+
+        self.assertTrue(contest.is_active)
+        self.assertTrue(contest.printing)
+        self.assertTrue(contest.is_printing_available)
+
+    @override_settings(PRINTING_AVAILABLE=True)
     def test_printing_before_start(self):
         """Contests should not allow printing when they are not started yet."""
-        pass
 
+        contest = Contest(
+            name="Test contest",
+            printing=True,
+            start_time=self.tomorrow,
+            freeze_time=self.tomorrow,
+            end_time=self.tomorrow
+        )
+
+        self.assertFalse(contest.is_active)
+        self.assertTrue(contest.printing)
+        self.assertFalse(contest.is_printing_available)
+
+    @override_settings(PRINTING_AVAILABLE=True)
     def test_printing_after_finish(self):
         """Contests should not allow printing when they are finished."""
-        pass
 
+        contest = Contest(
+            name="Test contest",
+            printing=True,
+            start_time=self.yesterday,
+            freeze_time=self.yesterday,
+            end_time=self.yesterday
+        )
+
+        self.assertFalse(contest.is_active)
+        self.assertTrue(contest.printing)
+        self.assertFalse(contest.is_printing_available)
+
+    @override_settings(PRINTING_AVAILABLE=False)
     def test_printing_global_disable(self):
         """Contests should not allow printing when it is globally disabled."""
-        pass
+
+        contest = Contest(
+            name="Test contest",
+            printing=True,
+            start_time=self.yesterday,
+            freeze_time=self.tomorrow,
+            end_time=self.tomorrow
+        )
+
+        self.assertTrue(contest.is_active)
+        self.assertTrue(contest.printing)
+        self.assertFalse(contest.is_printing_available)
+
+    def test_str(self):
+        """Contests should have a valid __str__ method."""
+        contest = Contest(
+            name="Test contest",
+        )
+        self.assertEquals(str(contest), contest.name)
+
+    def test_get_absolute_url(self):
+        """Contests should have a get_absolute_url
+        method poiting to a valid url."""
+
+        contest = Contest(
+            name="Test contest",
+        )
+        contest.save()
+
+        password = 'test'
+        admin = User.objects.create_superuser('test', 'test@t.com', password)
+        logged_in = self.client.login(username=admin.username,
+                password=password)
+        response = self.client.get(contest.get_absolute_url())
+
+        self.assertEquals(response.status_code, 200)
