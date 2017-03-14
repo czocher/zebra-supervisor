b3b1e5e055a800be477b27979ce3b8a19af984bc
Paweł Jan Czochański <czochanski@gmail.com>
2017-03-14 21:59:24 +0100

    Added tests for error_code_to_str templatetag

---
+++ judge/tests/test_templatetags.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase, override_settings
+
+from judge.templatetags.error_code_to_str import error_code_to_str
+
+from django.utils.translation import activate
+
+class TemplatetagsTestCase(TestCase):
+
+    def setUp(self):
+        # Disable translation for the tests
+        activate('en-en')
+
+    def test_error_code_to_known_codes(self):
+        """error_code_to_str should transform known error codes."""
+
+        self.assertTrue('Ok' in error_code_to_str(0))
+        self.assertTrue('Abnormal' in error_code_to_str(6))
+        self.assertTrue('Floating' in error_code_to_str(8))
+        self.assertTrue('Timelimit' in error_code_to_str(9))
+        self.assertTrue('Segmentation' in error_code_to_str(11))
+        self.assertTrue('Compilation' in error_code_to_str(127))
+
+    def test_error_code_to_str_unknown_codes(self):
+        """error_code_to_srt should not transform unknown error codes."""
+        self.assertEquals(100, error_code_to_str(100))
+        self.assertEquals(200, error_code_to_str(200))
